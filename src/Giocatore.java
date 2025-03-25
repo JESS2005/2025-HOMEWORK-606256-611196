df20f1d1923ede82c53a9f83317a646bd7160b8b
JESS2005 <darioferretti05@gmail.com>
2025-03-25 15:48:43 +0100

    Esercizio 2: refactoring

---
+++ src/Giocatore.java
@@ -0,0 +1,21 @@
+
+public class Giocatore {
+	private int cfu;
+	private Borsa borsa;
+	public Giocatore(int cfu) {
+		this.cfu=cfu;
+		borsa= new Borsa();
+	}
+	
+	public int getCfu() {
+		return this.cfu;
+	}
+
+	public void setCfu(int cfu) {
+		this.cfu = cfu;		
+	}
+	
+	public Borsa getBorsa() {
+		return this.borsa;
+	}
+}
