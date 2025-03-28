389eaf5fac9a6b9bf38c5eb8e6de460d1a387ffe
buonvecchiocarmine <car.salluzzi@stud.uniroma3.it>
2025-03-28 16:22:41 +0100

    test
    i test

---
+++ test/testStanza.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class testStanza {
+
+	private Stanza stanza;
+	@BeforeEach
+	void setUp() {
+		this.stanza=new Stanza("test");
+		this.stanza.impostaStanzaAdiacente("est", new Stanza("est"));
+		this.stanza.addAttrezzo(new Attrezzo("testatt", 2));
+	}
+	
+	
+	@Test
+	void testNome() {
+		assertTrue(this.stanza.getNome().equals("test"));
+	}
+	
+	@Test 
+	void testGetDirezioni() {
+		String[] dirtest = this.stanza.getDirezioni();
+		assertNotNull(dirtest);
+		assertTrue(dirtest.length==1);
+		assertTrue(dirtest[0].equals("est"));
+	}
+	
+	@Test
+	void testGetAttrezzo() {
+		assertNotNull(this.stanza.getAttrezzo("testatt"));
+		assertNull(this.stanza.getAttrezzo("ciao"));
+	}
+	
+	@Test
+	void testRemoveAttrezzo() {
+		Attrezzo a=this.stanza.getAttrezzo("testatt");
+		assertTrue(this.stanza.removeAttrezzo(a));
+		assertFalse(this.stanza.hasAttrezzo("testatt"));
+	}
+	
+	@Test
+	void testAddAttrezzo() {
+		assertTrue(this.stanza.addAttrezzo(new Attrezzo("testatt2", 5)));
+		assertNotNull(this.stanza.hasAttrezzo("testatt2"));
+	}
+}
