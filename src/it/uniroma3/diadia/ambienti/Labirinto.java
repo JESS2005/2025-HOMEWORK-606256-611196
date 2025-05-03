2b421096041020b9417fba8ad8e56b48ddd8caa7
buonvecchiocarmine <car.salluzzi@stud.uniroma3.it>
2025-05-03 19:25:11 +0200

    stanze magiche

--- src/it/uniroma3/diadia/ambienti/Labirinto.java
+++ src/it/uniroma3/diadia/ambienti/Labirinto.java
@@ -20,7 +20,7 @@ public class Labirinto {
 		Stanza atrio = new Stanza("Atrio");
 		Stanza aulaN11 = new Stanza("Aula N11");
 		Stanza aulaN10 = new Stanza("Aula N10");
-		Stanza laboratorio = new Stanza("Laboratorio Campus");
+		Stanza laboratorio = new StanzaMagica("Laboratorio Campus");
 		Stanza biblioteca = new Stanza("Biblioteca");
 		
 		/* collega le stanze */
