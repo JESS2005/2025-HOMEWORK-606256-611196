ebdd90aa58eb270f31d69765a2fb7065e20d4f65
buonvecchiocarmine <car.salluzzi@stud.uniroma3.it>
2025-05-05 12:00:38 +0200

    test comandoPrendi e comandoPosa

---
+++ test/testComandoPosa.java
@@ -0,0 +1,48 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.IO;
+import it.uniroma3.diadia.IOConsole;
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+import it.uniroma3.diadia.comandi.Comando;
+import it.uniroma3.diadia.comandi.ComandoPosa;
+import it.uniroma3.diadia.comandi.ComandoPrendi;
+import it.uniroma3.diadia.comandi.ComandoVai;
+
+public class testComandoPosa {
+
+	Partita partita;
+	Comando comando1 = null;
+	Comando comando2 = null;
+	IO io;
+	@BeforeEach
+	void setUp() {
+		partita=new Partita();
+		comando1 = new ComandoPrendi();
+		comando2 = new ComandoPosa();
+		io=new IOConsole();
+	}
+	
+	@Test
+	void testPosa() {
+		comando1.setParametro("osso");
+		comando1.esegui(partita, io);
+		comando2.setParametro("osso");
+		comando2.esegui(partita, io);
+		assertTrue(partita.getStanzaCorrente().hasAttrezzo("osso"));
+		
+	}	
+	
+	@Test
+	void testNonPosa() {
+		comando1.setParametro("osso");
+		comando1.esegui(partita, io);
+		comando2.setParametro("jojo");
+		comando2.esegui(partita, io);
+		assertFalse(partita.getStanzaCorrente().hasAttrezzo("jojo"));
+		
+	}	
+}
---
+++ test/testComandoPrendi.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.uniroma3.diadia.IO;
+import it.uniroma3.diadia.IOConsole;
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.ambienti.Stanza;
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+import it.uniroma3.diadia.comandi.Comando;
+import it.uniroma3.diadia.comandi.ComandoPrendi;
+import it.uniroma3.diadia.comandi.ComandoVai;
+
+public class testComandoPrendi {
+
+	Partita partita;
+	Comando comando = null;
+	IO io;
+	@BeforeEach
+	void setUp() {
+		partita=new Partita();
+		comando = new ComandoPrendi();
+		io=new IOConsole();
+	}
+	
+	@Test
+	void testPrendi() {
+		comando.setParametro("osso");
+		comando.esegui(partita, io);
+		assertTrue(partita.getGiocatore().getBorsa().hasAttrezzo("osso"));
+		
+	}	
+	
+	@Test
+	void testNonPrendi() {
+		comando.setParametro("GIORGIO");
+		comando.esegui(partita, io);
+		assertFalse(partita.getGiocatore().getBorsa().hasAttrezzo("GIORGIO"));
+		
+	}	
+}
